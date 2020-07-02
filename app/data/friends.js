ad19e16dc1a6edf01f040c1f37d84a9fbf6fad22
jrvillalba1993 <62047235+jrvillalba1993@users.noreply.github.com>
2020-07-01 20:21:05 -0400

    second repo test

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+// Variable to hold the array of friends objects data
+var friends = [{ "name": "Brooke", "photo": "https://static3.depositphotos.com/1000480/112/i/950/depositphotos_1122458-stock-photo-girl-in-a-field.jpg", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, { "name": "Skylar", "photo": "https://c8.alamy.com/comp/HRD2PC/charming-young-woman-in-pink-shirt-enjoying-the-sunlight-against-a-HRD2PC.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, { "name": "Andrea", "photo": "https://acidcow.com/pics/20130722/random_cute_girls_05.jpg", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
